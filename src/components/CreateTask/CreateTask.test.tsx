e8c406e6adde9770a9aec480b8b24999a63aa042
jhbosch <jesus.hbosch@gmail.com>
2024-04-21 02:29:40 -0400

    add jest-dom matchers to test and refactoring

--- src/components/CreateTask/CreateTask.test.tsx
+++ src/components/CreateTask/CreateTask.test.tsx
@@ -1,6 +1,7 @@
-import { afterEach, describe, expect, it } from 'vitest'
-import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, it } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react'
 import CreateTask from './CreateTask'
+import userEvent from '@testing-library/user-event'
 
 
 describe('create task', () => {
@@ -8,26 +9,18 @@ describe('create task', () => {
 
   it('should render component', () => {
     render(<CreateTask />)
-
   })
-
 
   it('should render input', () => {
     render(<CreateTask />)
     screen.getByRole('textbox')
   })
 
-  it('should write in input', () => {
+  it('should write "Hello" text', async () => {
     render(<CreateTask />)
     const input = screen.getByRole<HTMLInputElement>('textbox')
-
-    fireEvent.change(input, {
-      target : {
-        value: 'ABC'
-      }
-    })
-
-    expect(input.value).toBe('ABC')
+    await userEvent.type(input, 'Hello')
+    expect(input).toHaveValue('Hello')
   })
 
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -6,6 +8,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: "happy-dom",
+    setupFiles: ["./setupTest.ts"],
   }
 })
